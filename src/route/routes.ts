8166efb65cf15a6a621dfee1b3f807b4f519a451
harisshafi0902 <theharisshafi@gmail.com>
2023-08-11 17:06:46 +0500

    Revert "updated code structur & added routes"
    This reverts commit 41a44bfefa7bb9542bbe7944b0f5edc7dcfacc61.

--- src/route/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import UserController from '../controller/user.controller'; 
-
-const router = Router();
-const userController = new UserController();
-
-router.use('/users', userController.router);
-
-export default router;
